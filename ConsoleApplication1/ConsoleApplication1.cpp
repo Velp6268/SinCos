03efaf74f8e397476a79a93d242cb91efd10da82
bogor <bogor@DESKTOP-DC6RO5J>
2021-12-12 16:22:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>                                    
+#include <cmath>                                       
+#define PI 3.14159265    
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    double rate = 125; 
+
+ cout << "Cинус угла " << rate;
+ cout<< " градусов = " << sin(rate * PI / 180)<<endl;
+ cout << "Косинус угла " << rate;
+ cout << " градусов = " << cos(rate * PI / 180) << endl;
+ return 0;
+}
